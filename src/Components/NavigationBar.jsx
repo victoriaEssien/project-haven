bdd008afc6f9f938d005bae9e884cac5201d1d4b
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-02-28 01:07:32 +0100

    fixed routing issues

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -4,7 +4,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import { NavLink } from 'react-router-dom';
 
 function NavigationBar() {
 
@@ -21,9 +20,7 @@ function NavigationBar() {
             <Nav.Link href="#contact" className='nav-links'>Contact</Nav.Link>
           </Nav>
           <Nav className='ms-auto links'>
-          <Nav.Link>
-            <NavLink to='/coming' className='log-in-link'>Log in</NavLink>
-          </Nav.Link>
+          <Nav.Link as={Link} to="/coming" className='log-in-link'>Log in</Nav.Link>
           <Link to="/coming">
           <Button className='get-started-btn'>Get Started</Button>
           </Link>
