68c4f1c5abcd19ced036e83802901e224b8ff2b2
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-03-31 17:25:37 +0100

    added delete functionality

--- src/Components/TopicCard.jsx
+++ src/Components/TopicCard.jsx
@@ -1,15 +1,24 @@
 
 import { Link } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { faBookmark, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 function TopicCard({ topic }) {
   let color;
   const isMobileDevice = useMediaQuery({ query: '(max-width: 768px)' });
   const [isFilled, setIsFilled] = useState(true)
   const [bookmarkedData, setBookmarkedData] = useState([]);
+  const existingData = JSON.parse(localStorage.getItem("bookmarkedData")) || [];
+
+  useEffect(() => {
+    if(existingData.findIndex(item => item.id === topic.id) !== -1){
+      setIsFilled(true)
+    }else {
+      setIsFilled(false)
+    }
+  }, [existingData])
 
   const handleSaveTopic = (e) => {
     e.preventDefault()
@@ -22,11 +31,34 @@ function TopicCard({ topic }) {
 
     if (isPresent === -1) {
       bookmarkedDataCopy.push(topic)
+    }
+    setBookmarkedData(bookmarkedDataCopy)
+
+    if (existingData.findIndex(item => item.id === topic.id) !== -1) {
+      setIsFilled(true)
     } else {
+      setIsFilled(false)
+      localStorage.setItem("bookmarkedData", JSON.stringify([...existingData, ...bookmarkedDataCopy]))
+    }
+  }
+
+  const handleDeleteTopic = (e) => {
+    e.preventDefault()
+    setIsFilled(!isFilled)
+    let bookmarkedDataCopy = [...bookmarkedData]
+  
+    const isPresent = bookmarkedData.findIndex(
+      (item) => item.id === topic.id
+    )
+    
+    if (isPresent !== -1) {
       bookmarkedDataCopy.splice(isPresent, 1)
     }
     setBookmarkedData(bookmarkedDataCopy)
-    localStorage.setItem("bookmarkedData", JSON.stringify(bookmarkedDataCopy))
+    const existingDataCopy = JSON.parse(localStorage.getItem("bookmarkedData")) || []
+    const index = existingDataCopy.findIndex(item => item.id === topic.id);
+    existingDataCopy.splice(index, 1)
+    localStorage.setItem("bookmarkedData", JSON.stringify([...existingDataCopy]))
   }
 
   switch (topic.category) {
@@ -53,14 +85,14 @@ function TopicCard({ topic }) {
 
   let truncatedTitle = isMobileDevice ? topic.title.slice(0, 20).concat("...") : topic.title;
 
-  const truncatedDescription = topic.description.slice(0, 120).concat("...");
+  const truncatedDescription = topic.description.slice(0, 110).concat("...");
 
   return (
     <Link className="div-link" to={'/' + topic.id}>
       <div className="topic-card">
         <div className="icon-div">
           <p className="topic-category" style={{color: color}}>{topic.category}</p>
-          <FontAwesomeIcon className="icon" onClick={handleSaveTopic} icon={faBookmark} />
+          <FontAwesomeIcon className="icon" onClick={isFilled ? handleDeleteTopic : handleSaveTopic} icon={isFilled ? faXmark : faBookmark } color={isFilled ? '#41be80' : '#0d2040'} />
         </div>
         <h4 className="topic-title">{truncatedTitle}</h4>
         <p className="topic-desc">{truncatedDescription}</p>
